ae11e7994537a1ec51722dbe36e801e9e766f4b4
extsui <en422641@s.okayama-u.ac.jp>
2015-09-19 02:15:36 +0900

    MP3FileManagerクラスにDropbox機能を追加
    MP3FileManagerクラスのメンバに
    DropboxDownloaderを追加し，これを
    利用することで実現した．

--- bin/MP3FileManager.py
+++ bin/MP3FileManager.py
@@ -4,9 +4,15 @@ import os
 import random
 from collections import deque
 
+import DropboxDownloader
+from urllib3.exceptions import MaxRetryError
+
+import subprocess
+
 class MP3FileManager:
     __TOKEN_PATH    = '../token_store.txt'
     __DOWNLOAD_PATH = '../download/'
+    __TMP_PATH      = '../tmp/'
 
     SORT_BY_NAME   = 0
     SORT_BY_RANDOM = 1
@@ -14,13 +20,17 @@ class MP3FileManager:
     def __init__(self):
         self.__mp3_list = None
         self.__is_connected = False
+        self.dbd = DropboxDownloader.DropboxDownloader(self.__TOKEN_PATH)
 
     def download_list(self):
-        # ネットワークに接続できる場合
-        # file_list = ...
-        # ネットワークに接続できない場合
-        file_list = os.listdir(self.__DOWNLOAD_PATH)
-
+        try:
+            file_list = self.dbd.do_ls()
+            print('--- Network mode ---')
+            self.__is_connected = True
+        except MaxRetryError:
+            file_list = os.listdir(self.__DOWNLOAD_PATH)
+            print('--- Local mode ---')
+            self.__is_connected = False
         mp3_list = filter(lambda f: f.endswith('.mp3'), file_list)
         self.__mp3_list = deque(mp3_list)
 
@@ -36,22 +46,34 @@ class MP3FileManager:
         return self.__mp3_list[0]
 
     def next(self):
-        self.__mp3_list.rotate(1)
-
-    def prev(self):
         self.__mp3_list.rotate(-1)
 
-    def download_file(self):
-        dst_path = self.__DOWNLOAD_PATH + self.get()
-        print('download_file: ' + self.get())
-        print('dst_path: ' + dst_path)
-        # TODO: ファイルが存在しない場合ダウンロード
-        # ...
-        return dst_path
+    def prev(self):
+        self.__mp3_list.rotate(1)
 
-    def to_wav(self):
-        # TODO: *.mp3 --> *.wav
-        pass
+    def download_file(self):
+        from_path = self.get()
+        to_path = self.__DOWNLOAD_PATH + from_path
+        print('download_file: ' + to_path)
+        if not os.path.isfile(to_path):
+            if self.__is_connected:
+                self.dbd.do_get(from_path, to_path)
+            else:
+                print('BUG')
+        return to_path
+
+    def to_wave(self):
+        """ ../download/xxx.mp3 --> ../tmp/xxx.wav """
+        mp3_path = self.__DOWNLOAD_PATH + self.get()
+        wave_path = self.__TMP_PATH + os.path.basename(mp3_path).replace('.mp3', '.wav')
+        subprocess.call(['ffmpeg', '-i', mp3_path,
+                         '-ac', '1', '-ar', '44100', wave_path],
+                        stderr=-1)
+
+    # DEBUG
+    def play_wave(self):
+        wave_path = self.__TMP_PATH + self.get().replace('.mp3', '.wav')
+        subprocess.call(['aplay', '-q', wave_path])
 
 if __name__ == '__main__':
     mp3 = MP3FileManager()
