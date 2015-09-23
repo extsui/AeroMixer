5636af0a97451d3e676d4c3c0a6445c165aca4f3
extsui <en422641@s.okayama-u.ac.jp>
2015-09-23 14:06:36 +0900

    状態遷移の追加と起動スクリプトの作成

--- bin/AeroMixer.py
+++ bin/AeroMixer.py
@@ -6,7 +6,7 @@ import STFTAudio as STFT
 import sys
 import exceptions
 
-def read_timeout(timeout):
+def read_timeout(timeout=None):
     from select import select
     rlist, _, _ = select([sys.stdin], [], [], timeout)
     if rlist:
@@ -14,41 +14,80 @@ def read_timeout(timeout):
     else:
         return None
 
-stft = STFT.STFTAudio()
-mp3 = MP3.MP3FileManager()
+STATE_INIT    = 0
+STATE_SELECT  = 1
+STATE_PREPARE = 2
+STATE_PLAY    = 3
+STATE_STOP    = 4
+STATE_FINISH  = 5
+STATE_QUIT    = 6
 
-mp3.download_list()
-mp3.sort(MP3.MP3FileManager.SORT_BY_NAME)
+state = STATE_INIT
+
+print('AeroMixer: start')
 
 while True:
-    while True:
+
+    if state == STATE_INIT:
+        stft = STFT.STFTAudio()
+        mp3 = MP3.MP3FileManager()
+        mp3.download_list()
+        mp3.sort(MP3.MP3FileManager.SORT_BY_NAME)
+        state = STATE_SELECT
+
+    elif state == STATE_SELECT:
         print('[' + mp3.get() + ']')
-        s = raw_input('(+|-|s|q)> ')
+        sys.stderr.write('(+|-|s|q)> ')
+        s = read_timeout()
         if s == '+':
             mp3.next()
         elif s == '-':
             mp3.prev()
         elif s == 's':
-            break
+            state = STATE_PREPARE
         elif s == 'q':
-            raise exceptions.NotImplementedError('quit')
+            state = STATE_QUIT
 
-    print('select: ' + mp3.get())
-    mp3.download_file()
+    elif state == STATE_PREPARE:
+        print('download')
+        mp3.download_file()
+        print('convert')
+        stft.open(mp3.to_wave())
+        print('play')
+        stft.start()
+        state = STATE_PLAY
 
-    stft.open(mp3.to_wave())
-
-    print('playing...')
-    stft.start()
-
-    while stft.is_playing() or stft.is_stopping():
+    elif state == STATE_PLAY:
+        if not stft.is_playing():
+            state = STATE_FINISH
         s = read_timeout(timeout=0.100)
-        if stft.is_playing() and s is None:
+        if s is None:
             print(stft.stft())
         elif s == 's':
-            stft.start()
-        elif s == 'p':
             stft.stop()
+            state = STATE_STOP
+        elif s == 'q':
+            state = STATE_QUIT
 
-    stft.close()
-    mp3.rm_wave()
+    elif state == STATE_STOP:
+        s = read_timeout()
+        if s == 's':
+            stft.start()
+            state = STATE_PLAY
+        elif s == 'q':
+            state = STATE_QUIT
+
+    elif state == STATE_FINISH:
+        stft.close()
+        mp3.rm_wave()
+        state = STATE_SELECT
+
+    elif state == STATE_QUIT:
+        # STOPからQUITになった場合，tmp/にwavファイルが残るけど，
+        # アンマウント時に削除されるので問題ない．
+        break
+
+    else:
+        raise exceptions.SystemError('STATE ERROR')
+
+print('AeroMixer: exit')
--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+echo 'sh: mount ./tmp'
 sudo mount -t tmpfs -o size=256m /dev/shm ./tmp
+
+echo 'sh: start'
+cd bin
+./AeroMixer.py
+cd ..
+
+echo 'sh: umount ./tmp'
+sudo umount ./tmp
